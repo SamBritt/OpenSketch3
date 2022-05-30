dd6fc76db7f3665c963081a6064d4cee4ecd3a03
SamBritt <sam.britt2@yahoo.com>
2022-05-30 16:46:18 -0500

    carousel scrolling

--- src/components/gallery/SingleCarousel.js
+++ src/components/gallery/SingleCarousel.js
@@ -1,15 +1,38 @@
 import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/outline'
+import { useRef, useState } from 'react'
+import { useInView } from 'react-intersection-observer'
 import './singleCarousel.scss'
 
 const SingleCarousel = ({ images }) => {
 
+    const [scrollState, setScrollState] = useState(0)
+    const [scrollFactor, setScrollFactor] = useState(1)
+
+    const { ref, inView, entry } = useInView({
+        /* Optional options */
+        threshold: 0,
+      });
+
+    const scrollerRef = useRef()
+    const parentRef = useRef()
+
+    const scrollRight = () => {
+        let scrollPercentage = 100 * scrollerRef.current.scrollLeft / (scrollerRef.current.scrollWidth-scrollerRef.current.clientWidth); 
+
+        scrollerRef.current.scrollBy({left: parentRef.current.offsetWidth, behavior: 'smooth'})
+    }
+
+    const scrollLeft = () => {
+        scrollerRef.current.scrollBy({left: -parentRef.current.offsetWidth, behavior: 'smooth'})
+    }
+
     return (
-        <div className='relative scroller h-48 overflow-x-auto'>
+        <div ref={parentRef} className='relative h-48 overflow-x-auto'>
             <div className='absolute transition-all ease duration-200 opacity-0 hover:opacity-100 flex items-center justify-center pl-4 z-20 h-full w-20 bg-gradient-to-r from-stone-900'>
-                <ArrowLeftIcon className='h-5 w-5 text-stone-200 transition-all ease duration-200 hover:scale-150'/>
+                <ArrowLeftIcon onClick={scrollLeft} className='h-5 w-5 text-stone-200 transition-all ease duration-200 hover:scale-150'/>
             </div>
 
-            <section className='scroller grid grid-rows-1 grid-flow-col auto-cols-min h-48 gap-x-2 overflow-x-auto'>
+            <section ref={scrollerRef} className='scroller grid grid-rows-1 grid-flow-col auto-cols-min h-48 gap-x-2 overflow-x-auto'>
                 {
                     images && images.map((item) => (
                         <div
@@ -22,7 +45,7 @@ const SingleCarousel = ({ images }) => {
             </section>
 
             <div className='absolute transition-all ease duration-200 opacity-0 hover:opacity-100 flex justify-center items-center pr-4 z-20 top-0 right-0 h-full w-20 bg-gradient-to-l from-stone-900'>
-                <ArrowRightIcon className='h-5 w-5 text-stone-200 transition-all ease duration-200 hover:scale-150'/>
+                <ArrowRightIcon onClick={scrollRight} className='h-5 w-5 text-stone-200 transition-all ease duration-200 hover:scale-150'/>
             </div>
         </div>
     )
